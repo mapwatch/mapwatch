27282d24dcd9f20a1a2e25ee6109b6bbcd0b7058
Evan <github@erosson.org>
2018-05-18 19:48:09 -0400

    travis

--- build.sh
+++ build.sh
@@ -2,8 +2,6 @@
 cd "`dirname "$0"`"
 rm -rf ./dist
 mkdir -p ./dist
-ls .
-ls ./src
 cat ./src/index.html | HOST='' envsubst > dist/index.html
 cp ./ports.js ./dist/
 elm-make src/Main.elm --output=dist/elm.js
