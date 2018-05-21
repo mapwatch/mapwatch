b8942de2fd852c69a60237c746380d2eb90aa8a3
Evan <github@erosson.org>
2018-05-21 04:03:14 -0400

    fix build script

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ cd "`dirname "$0"`"
 dest=./dist
 rm -rf $dest
 mkdir -p $dest
-cp ./src/ports.js ./src/main.css ./src/index.html ./examples $dest
+cp -r ./src/ports.js ./src/main.css ./src/index.html ./examples $dest
 elm-make src/Main.elm --output=$dest/elm.js
