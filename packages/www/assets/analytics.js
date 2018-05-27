a50d77892a74c5ac25ae3c37ede2383ffc84b98e
Evan <github@erosson.org>
2018-05-27 09:36:53 -0400

    refactor analytics loading code, enable ip anonymization

--- packages/www/assets/analytics.js
+++ packages/www/assets/analytics.js
@@ -1,4 +1,4 @@
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
-gtag('config', 'UA-119582500-1');
+gtag('config', 'UA-119582500-1', {'anonymize_ip': true});
