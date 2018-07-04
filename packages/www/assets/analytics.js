febc1924fcd5de95cdcb9aee347635caee9ee3a9
Evan <github@erosson.org>
2018-07-04 11:19:31 -0400

    analytics beacon

--- packages/www/assets/analytics.js
+++ packages/www/assets/analytics.js
@@ -1,4 +1,5 @@
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
+gtag('set', 'transport', 'beacon');
 gtag('config', 'UA-119582500-1', {'anonymize_ip': true});
