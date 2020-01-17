b9428deedaad406fbc745cb21d0ec4d9d34f66ce
Evan <git@erosson.org>
2020-01-17 17:18:50 -0500

    hidden link to debug page; clean up debug page

--- packages/www/src/View/Debug.elm
+++ packages/www/src/View/Debug.elm
@@ -5,15 +5,19 @@ import Html.Attributes as A exposing (..)
 import Html.Events as E exposing (..)
 import Model exposing (OkModel)
 import Route exposing (Route)
+import View.Home exposing (viewHeader)
+import View.Nav
 
 
 view : OkModel -> Html msg
 view model =
-    div []
-        [ ul []
-            [ li [] [ a [ Route.href (Route.Timer Route.timerParams0) ] [ text "home" ] ]
-            , li [] [ a [ Route.href Route.DebugDatamine ] [ text "/debug/datamine" ] ]
-            , li [] [ a [ Route.href Route.DebugDumpLines ] [ text "/debug/dumplines" ] ]
+    div [ class "main" ]
+        [ viewHeader
+        , View.Nav.view Nothing
+        , h5 [] [ text "secret debugging tools" ]
+        , ul []
+            [ li [] [ a [ Route.href Route.DebugDatamine ] [ text "/debug/datamine" ] ]
+            , li [] [ a [ Route.href Route.DebugDumpLines ] [ text "/debug/dumplines (broken)" ] ]
             ]
         , p [] [ text "Platform: ", code [] [ text model.flags.platform ] ]
         , p [] [ text "Version: ", code [] [ text model.flags.version ] ]
--- packages/www/src/View/Settings.elm
+++ packages/www/src/View/Settings.elm
@@ -18,6 +18,5 @@ view model =
         , View.Volume.view model
         , button [ onClick (Model.Reset (Just Route.root)) ] [ text "Analyze another Client.txt log file" ]
         , p [] [ text "Mapwatch is open source! ", a [ target "_blank", href "https://www.github.com/mapwatch/mapwatch" ] [ text "View the source code." ] ]
-
-        -- , div [ class "debug-link" ] [ a [ Route.href Route.Debug ] [ text "secret debugging tools" ] ]
+        , div [ class "debug-link" ] [ a [ Route.href Route.Debug ] [ text "secret debugging tools" ] ]
         ]
