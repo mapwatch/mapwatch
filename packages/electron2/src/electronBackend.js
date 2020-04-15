8d969059b9adb4051ba2c2c4aa635b2331abdc46
Evan <git@erosson.org>
2020-04-15 09:02:53 -0400

    more steam autoload paths. fixes #73

--- packages/electron2/src/electronBackend.js
+++ packages/electron2/src/electronBackend.js
@@ -23,12 +23,15 @@ function ElectronBackend() {
       "C:\\Program Files (x86)\\Grinding Gear Games\\Path of Exile\\logs\\Client.txt",
       "C:\\Program Files\\Grinding Gear Games\\Path of Exile\\logs\\Client.txt",
       "C:\\Steam\\steamapps\\common\\Path of Exile\\logs\\Client.txt",
+      "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Path of Exile\\logs\\Client.txt",
+      "C:\\Program Files\\Steam\\steamapps\\common\\Path of Exile\\logs\\Client.txt",
     ].map(path =>
       fs.access(path)
       .then(() => path)
       .catch(err => null)
     ))
     .then(paths => paths.filter(val => !!val))
+    // .then(paths => {console.log('autoOpen paths', paths); return paths})
     .then(paths =>
         paths.length
           ? Promise.resolve(paths[0])
