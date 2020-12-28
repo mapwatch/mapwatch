8604c01472744ac6eeff86a00e2ed188b487ac24
Evan Rosson <git@erosson.org>
2020-12-28 00:41:37 -0500

    more work on steamctl auth. #146

--- steamctl-auth.py
+++ steamctl-auth.py
@@ -14,50 +14,77 @@
 import pexpect
 import sys
 import os
+import time
 
 def printcmd(str):
-    print("\n+", str)
+    print("\n+", str, flush=True)
     return str
 
 def authenticator_remove(steamctl, user):
     # Remove existing authenticator, if any
     cmd = printcmd(f"{steamctl} authenticator remove --force {user}")
-    output = pexpect.run(cmd)
-    print(output)
+    output, exitstatus = pexpect.run(cmd, timeout=5, withexitstatus=True)
+    print(output.decode('utf-8'), flush=True)
 
 def authenticator_add(steamctl, user, passwd, secret):
     # I don't like putting the secret in the command line like this - `ps` could see it.
     # But I'm running this on private CI machines, so it *should* be okay, I think.
     # Also, at least the password is still obscured.
     cmd = printcmd(f"{steamctl} authenticator add --from-secret {secret} {user}")
-    child = pexpect.spawn(cmd)
+    child = pexpect.spawn(cmd, timeout=5)
     child.logfile_read = sys.stdout.buffer
-    index = child.expect(["Enter password for '.*':", pexpect.EOF])
+    index = child.expect(["Enter password for .*:", pexpect.EOF])
+    print("\nauth_add expected", index)
     if index == 0:
-        child.send(passwd)
-    # child.expect("Authenticator added successfully")
+        child.send(passwd+"\n")
+    child.expect("Authenticator added successfully")
     child.read()
+    child.close()
+    if (child.exitstatus):
+        raise Exception("steamctl exitstatus: "+str(child.exitstatus))
 
 def authenticator_code(steamctl, user):
     cmd = printcmd(f"{steamctl} authenticator code {user}")
-    return pexpect.run(cmd)
+    output, exitstatus = pexpect.run(cmd, timeout=5, withexitstatus=True)
+    if (exitstatus):
+        raise Exception("steamctl exitstatus: "+str(exitstatus))
+    return output.decode('utf-8').strip()
 
 def login_with_2fa(steamctl, user, passwd):
-    cmd = printcmd(f"{steamctl} --user {user} depot info -a 440")
-    child = pexpect.spawn(cmd)
+    cmd = printcmd(f"{steamctl} --user {user} depot info -a 238960")
+    child = pexpect.spawn(cmd, timeout=30)
     child.logfile_read = sys.stdout.buffer
     index = child.expect(["Password:", pexpect.EOF])
     if index == 0:
-        child.send(passwd)
-        index = child.expect([".*2FA.*:", pexpect.EOF])
+        child.send(passwd+"\n")
+        index = child.expect(["Enter 2FA code:", pexpect.EOF])
         if index == 0:
-            child.send(authenticator_code())
+            time.sleep(5)
+            code = authenticator_code(steamctl=steamctl, user=user)
+            print("authenticator code 1:", str(code), flush=True)
+            time.sleep(3)
+            child.send(code+"\n")
+            # retry once
+            index = child.expect(["Incorrect code. Enter 2FA code:", pexpect.EOF])
+            if index == 0:
+                time.sleep(10)
+                code = authenticator_code(steamctl=steamctl, user=user)
+                print("authenticator code 2:", str(code), flush=True)
+                time.sleep(3)
+                # retry
+                child.send(code+"\n")
+                child.expect(pexpect.EOF)
     child.read()
+    child.close()
+    if (child.exitstatus):
+        raise Exception("steamctl exitstatus: "+str(child.exitstatus))
 
 def verify_auth_remembered(steamctl):
-    cmd = printcmd(f"{steamctl} depot info -a 440")
-    output = pexpect.run(cmd)
-    print(output)
+    cmd = printcmd(f"{steamctl} depot info -a 238960")
+    output, exitstatus = pexpect.run(cmd, timeout=5, withexitstatus=True)
+    print(output.decode('utf-8'), flush=True)
+    if (exitstatus):
+        raise Exception("steamctl exitstatus: "+str(exitstatus))
 
 def main():
     # Steam account login
@@ -72,7 +99,7 @@ def main():
 
     authenticator_remove(steamctl=steamctl, user=user)
     authenticator_add(steamctl=steamctl, user=user, passwd=passwd, secret=secret)
-    authenticator_code(steamctl=steamctl, user=user)
+    #authenticator_code(steamctl=steamctl, user=user)
     login_with_2fa(steamctl=steamctl, user=user, passwd=passwd)
     verify_auth_remembered(steamctl=steamctl)
 
