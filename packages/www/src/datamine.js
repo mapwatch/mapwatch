db5b0206828641b3b05630547859d0c4bae8bcbe
Evan Rosson <github@erosson.org>
2022-08-16 17:48:49 +0000

    use new leagues file

--- packages/www/src/datamine.js
+++ packages/www/src/datamine.js
@@ -1,6 +1,6 @@
 // no `import` statements, this is used by both `tests/elm-string-from-file.js` and `src/index.js`
 const datamine = require('../../datamine2/dist/mapwatch.json')
-const leagues = require('../../datamine/dist/leagues.json')
+const leagues = require('../../datamine2/dist/leagues.json')
 const atlasbase = require('../../datamine/dist/atlasbase.json')
 const divcards = require('../../datamine/dist/divcards.json')
 const ultimatum = require('../../datamine/UltimatumModifiers.json')
